6e2455f90f0917bb79b177ed27f1f5c512145bea
AswinP-Thampan <aswinp.thampan@gmail.com>
2024-11-18 22:46:41 +0530

    pages 1

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -2,23 +2,23 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-<header class="fixed flex flex-wrap md:justify-start md:flex-nowrap w-full bg-black text-sm py-3 font-F1R">
-      <nav class="max-w-[85rem] w-full mx-auto px-4 md:flex md:items-center md:justify-between">
-    <div class="flex items-center justify-between">
-      <a class="flex-none text-xl text-red-600 font-F1W " href="#" aria-label="Brand">
+<header className="fixed flex flex-wrap md:justify-start md:flex-nowrap w-full bg-black text-sm py-3 font-F1R">
+      <nav className="max-w-[85rem] w-full mx-auto px-4 md:flex md:items-center md:justify-between">
+    <div className="flex items-center justify-between">
+      <a className="flex-none text-xl text-red-600 font-F1W " href="#" aria-label="Brand">
         eaf1
       </a>
-      <div class="md:hidden">
-        <button type="button" class="hs-collapse-toggle relative size-7 flex justify-center items-center gap-x-2 rounded-lg bg-red-600 text-gray-800 focus:outline-none disabled:opacity-50 disabled:pointer-events-none" id="hs-navbar-example-collapse" aria-expanded="false" aria-controls="hs-navbar-example" aria-label="Toggle navigation" data-hs-collapse="#hs-navbar-example">
-          <svg class="hs-collapse-open:hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
-          <svg class="hs-collapse-open:block hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+      <div className="md:hidden">
+        <button type="button" className="hs-collapse-toggle relative size-7 flex justify-center items-center gap-x-2 rounded-lg bg-red-600 text-gray-800 focus:outline-none disabled:opacity-50 disabled:pointer-events-none" id="hs-navbar-example-collapse" aria-expanded="false" aria-controls="hs-navbar-example" aria-label="Toggle navigation" data-hs-collapse="#hs-navbar-example">
+          <svg className="hs-collapse-open:hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
+          <svg className="hs-collapse-open:block hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
         </button>
       </div>
     </div>
-    <div id="hs-navbar-example" class="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow md:block" aria-labelledby="hs-navbar-example-collapse">
-      <div class="flex flex-col gap-3 mt-5 md:flex-row md:items-center md:justify-end md:mt-0 md:ps-5">
-        <a className="md:p-4 py-2 block text-red-600 font-semibold" href="#">Home</a>
-        <a className="md:p-4 py-2 block text-white hover:text-red-600 font-semibold" href="#">McLaren</a>
+    <div id="hs-navbar-example" className="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow md:block" aria-labelledby="hs-navbar-example-collapse">
+      <div className="flex flex-col gap-3 mt-5 md:flex-row md:items-center md:justify-end md:mt-0 md:ps-5">
+        <a className="md:p-4 py-2 block text-white hover:text-red-600 font-semibold" href="/">Home</a>
+        <a className="md:p-4 py-2 block text-white hover:text-red-600 font-semibold" href="/mclaren">McLaren</a>
         <a className="md:p-4 py-2 block text-white hover:text-red-600 font-semibold" href="#">Red Bull</a>
         <a className="md:p-4 py-2 block text-white hover:text-red-600 font-semibold" href="#">Mercedes</a>
         <a className="md:p-4 py-2 block text-white hover:text-red-600 font-semibold" href="#">Aston Martin</a>
--- src/app/page.js
+++ src/app/page.js
@@ -15,25 +15,25 @@ export default function Home() {
               &nbsp; Everything about F1&nbsp;
               {/* <span className="font-F1W ">F1</span> */}
             </div>
-            <p>
+            <div>
               "eaf1" is a passion project dedicated to Formula 1. As a new F1 fan myself, I found it challenging to understand the basics and discover what truly captivates people about the sport. This website is my attempt to break down those fundamentals and share some of the fascinating aspects that drew me in. Here, I'll explore the key elements and insights that make F1 so thrilling for fans worldwide.
-            </p>
+            </div>
             <b className="text-red-600 font-F1W text-sm">What is Formula 1?</b>
 
-            <p>
+            <div>
               Formula 1 (F1) is the highest class of international single-seater auto racing, governed by the Fédération Internationale de l'Automobile (FIA). It involves a series of races known as Grand Prix, held across the world on specially designed circuits or closed city streets. F1 cars are engineering marvels—lightweight, aerodynamically sophisticated, and capable of reaching speeds over 200 mph (322 km/h).
-            </p>
+            </div>
 
 
             <b className="text-red-600 font-F1W text-sm">Why is F1 Popular?</b>
-            <p>
+            <div>
               F1’s popularity comes from its unique mix of high-tech innovation, skill, and danger. It’s a global sport with a rich history, and each season is filled with intrigue, from intense rivalries and drama on and off the track to breakthrough technological advancements.
-            </p>
+            </div>
 
             {/* <b className="text-red-600 font-F1W text-sm">What and Who is FIA?</b>
-            <p>
+            <div>
               The FIA stands for the <b>Fédération Internationale de l'Automobile.</b> It's the governing body for world motorsport and is responsible for setting rules, regulations, and standards for various forms of racing, including Formula 1. Founded in 1904, the FIA oversees the organization, safety, and integrity of international motorsport events, and promotes responsible and sustainable mobility globally.
-            </p>
+            </div>
             <ol className="list-inside list-decimal">In Formula 1, the FIA plays a crucial role in:
               <li className="mb-2 mt-4"><b>Setting and enforcing regulations:</b> The FIA sets technical and sporting rules that teams and drivers must follow.</li>
               <li className="mb-2"><b>Ensuring safety:</b> The FIA continuously works on improving safety for drivers, teams, and spectators through innovations and strict safety protocols.</li>
@@ -43,11 +43,11 @@ export default function Home() {
 
 
             <b className="text-red-600 font-F1W text-sm">How does one win in F1</b>
-            <p>
+            <div>
               Winning in F1 can mean <b>winning a single race</b> or aiming for the season-long goal of winning the <b>Driver’s Championship</b> or <b>Constructor’s Championship.</b>
-              <p className="text-red-600 text-xs text-right pr-8">Learn More ...</p>
-            </p>
-            <p></p>
+              <div className="text-red-600 text-xs text-right pr-8">Learn More ...</div>
+            </div>
+            <div></div>
           </main>
           {/* </div> */}
 
