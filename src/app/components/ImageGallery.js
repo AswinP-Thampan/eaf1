87dcd794e167c54e96cc7a619e49855be426d22a
AswinP-Thampan <aswinp.thampan@gmail.com>
2024-11-20 01:09:42 +0530

    mclaren content addition

--- src/app/components/ImageGallery.js
+++ src/app/components/ImageGallery.js
@@ -38,24 +38,24 @@ export default function ImageGallery(imgArr) {
 
     return (
         <div className="p-8 relative">
-            <div className="grid grid-cols-[2%_75%_7%_2%] gap-4 justify-center">
-                <div className='flex justify-center'>
-                    <button onClick={() => MoveLeft()} type="button" className="flex items-center justify-center h-full cursor-pointer group focus:outline-none" data-carousel-prev>
+            <div className="grid grid-cols-[2%_75%_8%_2%] gap-4 justify-center">
+                <div className='flex justify-center items-center'>
+                    <button onClick={() => MoveLeft()} type="button" className="flex items-center justify-center h-fit cursor-pointer group focus:outline-none" data-carousel-prev>
                         <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
                         </svg>
                     </button>
                 </div>
                 <div className='flex justify-center  '>
-                    <img className={`h-auto rounded-3xl ${fadeClass} border-8 border-l-0 border-b-0 border-mclaren_orange`} src={imgArr.imgArr[0]} alt="Main Image" />
+                    <img className={`h-fit rounded-3xl ${fadeClass} border-8 border-l-0 border-b-0 border-mclaren_orange`} src={imgArr.imgArr[0]} alt="Main Image" />
                 </div>
                 <div className="grid grid-cols-1 gap-2">
                     {imgArr.imgArr.slice(1).map((item, index) =>
                         <img className={`h-auto max-w-full rounded-lg ${fadeClass} hover:opacity-50`} key={index} src={item} alt={`Image ${index}`} style={{ cursor: 'pointer' }} onClick={() => swapPics(index)} />
                     )}
                 </div>
-                <div className='flex justify-center'>
-                    <button onClick={() => MoveRight()} type="button" className="flex items-center justify-center h-full cursor-pointer group focus:outline-none" data-carousel-next>
+                <div className='flex justify-center items-center'>
+                    <button onClick={() => MoveRight()} type="button" className="flex items-center justify-center h-fit cursor-pointer group focus:outline-none" data-carousel-next>
                         <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
                         </svg>
--- src/app/mclaren/page.js
+++ src/app/mclaren/page.js
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react'
 import NavBar from '../components/NavBar'
 import ImageGallery from '../components/ImageGallery'
+import Table from '../components/Table'
 
 export default function Page() {
 
@@ -16,28 +17,87 @@ export default function Page() {
         `${f1Url}/MCL60-LN4-Overhead-7K`,
         `${f1Url}/MCL60-LN4-Side-7K`,
         `${f1Url}/MCL60-LN4-Rear-Low-7K`,
-        // `${amalgamCollectionUrl}/DSCF6455_4000x2677_crop_center.jpg`,
-        // `${amalgamCollectionUrl}/DSCF6445_4000x2677_crop_center.jpg`,
+        `${amalgamCollectionUrl}/DSCF6455_4000x2677_crop_center.jpg`,
+        `${amalgamCollectionUrl}/DSCF6445_4000x2677_crop_center.jpg`,
         // `${amalgamCollectionUrl}/DSCF6510_4000x2677_crop_center.jpg`,
         // `${amalgamCollectionUrl}/DSCF6451_01950db2-dfb6-44dd-b05a-8eb9f5f01ad2_4000x2677_crop_center.jpg`,
 
         `${f1Url}/3`,
         `${f1Url}/6`,
-        `${f1Url}/7`,
+        // `${f1Url}/7`,
     ]);
+
+    const chasisInfo = [
+        {
+            chasis: "Power Unit type",
+            info: "Mercedes-AMG M14 E Performance  "
+        },
+        {
+            chasis: "Monocoque",
+            info: "Carbon-fibre composite, incorporating driver controls and fuel cell  "
+        },
+        {
+            chasis: "Safety structures",
+            info: "Cockpit survival cell incorporating impact resistant construction and anti-penetration panels, front impact structure, prescribed side impact structures, integrated rear impact structure, front and rear roll structures, halo secondary roll structure, driver headrest protection and safety seat  "
+        },
+        {
+            chasis: "Bodywork",
+            info: "Carbon-fibre composite, including engine cover, sidepods, floor, nose, front wing and rear wing with driver-operated drag reduction system"
+        },
+        {
+            chasis: "Front suspension",
+            info: "Carbon-fibre / titanium suspension legs, pullrod operating inboard torsion bar, spring and damper system"
+        },
+        {
+            chasis: "Rear suspension",
+            info: "Carbon-fibre / titanium suspension legs, pullrod operating inboard torsion bar, spring and damper system"
+        },
+        {
+            chasis: "Weight",
+            info: "Overall vehicle weight 798 kg (including driver, excluding fuel)  "
+        },
+        {
+            chasis: "Weight distribution between ",
+            info: "44.6% and 46.1% on front axle  "
+        },
+        {
+            chasis: "Electronics",
+            info: "McLaren Applied. Including chassis control, power unit control, data acquisition, sensors, data analysis  "
+        },
+        {
+            chasis: "Brake system",
+            info: "Six piston brake calipers with front / rear master cylinders. ‘Brake by wire’ rear brake control system. Carbon-carbon ventilated discs and pads  "
+        },
+        {
+            chasis: "Tyres",
+            info: "Pirelli P Zero F1. Now with an additional compound, the C0 – bringing our total to six compounds in the pool. Pirelli select three for each event. "
+        },
+    ]
 
     const updateState = (imgArr) => {
         setImageArr(imgArr)
     }
 
     return (
-        <div>
+        <div >
             <div>
                 <NavBar />
             </div>
-            <div className='flex justify-center items-center'>
+            <div className='flex flex-col justify-center items-center mt-8'>
+                <b className=" text-4xl text-mclaren_orange font-F1W">McLaren</b>
                 <ImageGallery imgArr={ImageArr} handleState={updateState} />
             </div>
+            <div className='font-F1R flex justify-center pb-8'>
+                <div className='w-10/12'>
+                    <div>
+                        The McLaren MCL60 is a Formula One race car designed and built by McLaren to compete in the 2023 Formula One World Championship, driven by Lando Norris and rookie Oscar Piastri
+                    </div>
+                    <div>
+                        Lets have a deeper look into the MCL60:
+                    </div>
+                </div>
+            </div>
+            <Table chasisInfo={chasisInfo} />
         </div>
     )
 }
