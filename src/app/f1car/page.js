5ff5b3fb1e8f2d3537df88b81eebed4e8d31c88d
AswinP-Thampan <aswinp.thampan@gmail.com>
2024-11-23 04:45:43 +0530

    knowMore 2

--- src/app/f1car/page.js
+++ src/app/f1car/page.js
@@ -17,7 +17,7 @@ export default function page() {
         setType(type)
     }
 
-    const handleGoBack = () =>{
+    const handleGoBack = () => {
         setType('Click to know more')
         setSrc('/basic/b1.jpg')
     }
@@ -36,28 +36,27 @@ export default function page() {
                     <div className='flex flex-col gap-4 font-F1R text-lg'>
                         <div>Welcome to the interactive F1 car exploration! Hover over different parts of the car to discover key components and their functions. Each section, from the Halo Device to the Tyres, plays a crucial role in the performance, safety, and speed of the car.</div>
                         <div>Simply move your mouse over the labeled areas, and click it to get a detailed information about that specific part. Whether you're curious about the aerodynamics of the spoilers or the power behind the engine, this interactive guide will give you a deeper understanding of what makes an F1 car a marvel of engineering.</div>
-                 </div>
+                    </div>
                 </div>
                 <div className='relative' style={{ position: 'relative', width: '100%', maxWidth: '1200px', margin: '0 auto' }}>
-                   {type !== 'Click to know more' &&
-                     <button onClick={() => handleGoBack()} type="button" className='bg-transparent hover:bg-opacity-50 absolute top-[7%] left-[5%] w-8 h-8 sm:w-12 sm:h-12 lg:w-14 lg:h-14 transform -translate-x-1/2 -translate-y-1/2' >
-                     <img src='/icons/back.svg' className='w-7 h-7'/>
-                 </button>
-                   }
+                    {type !== 'Click to know more' &&
+                        <button onClick={() => handleGoBack()} type="button" className='bg-transparent hover:bg-opacity-50 absolute top-[7%] left-[5%] w-8 h-8 sm:w-12 sm:h-12 lg:w-14 lg:h-14 transform -translate-x-1/2 -translate-y-1/2' >
+                            <img src='/icons/back.svg' className='w-7 h-7' />
+                        </button>
+                    }
                     <img className={`h-fit rounded-3xl w-full border-8 border-l-0 border-b-0 border-color-`} src={src} alt="Main Image" />
                     {type == 'Click to know more' &&
                         <div>
                             <button data-tooltip-target="tooltip-animation" onClick={() => handleButtonClick('Halo')} type="button" className={` ${buttonStyle} top-[24%] left-[55%]`} />
-                    <button data-tooltip-target="tooltip-animation" onClick={() => handleButtonClick('TopAirIntake')} type="button" className={` ${buttonStyle} top-[18%] left-[61.75%]`} />
-                    <button data-tooltip-target="tooltip-animation" onClick={() => handleButtonClick('SideAirIntake')} type="button" className={` ${buttonStyle} top-[38%] left-[40%]`} />
-                    <button data-tooltip-target="tooltip-animation" onClick={() => setType('Suspension')} type="button" className={` ${buttonStyle} top-[46%] left-[29%]`} />
-                    <button data-tooltip-target="tooltip-animation" onClick={() => setType('Brake Cooling Duct')} type="button" className={` ${buttonStyle} top-[52%] left-[23.5%]`} />
-                    <button data-tooltip-target="tooltip-animation" onClick={() => handleButtonClick('Tyres')} type="button" className={` ${buttonStyle} top-[52%] left-[60%]`} />
-                    <button data-tooltip-target="tooltip-animation" onClick={() => setType('Front Wing')} type="button" className={` ${buttonStyle} top-[82%] left-[42%]`} />
-                    <button data-tooltip-target="tooltip-animation" onClick={() => setType('Rear Wing')} type="button" className={` ${buttonStyle} top-[10%] left-[85%]`} />
-                    <button data-tooltip-target="tooltip-animation" onClick={() => setType('DRS')} type="button" className={` ${buttonStyle} top-[13%] left-[75%]`} />
-                    <button data-tooltip-target="tooltip-animation" onClick={() => setType('Double Barrel')} type="button" className={` ${buttonStyle} top-[24%] left-[77%]`} />
-                            </div>
+                            <button data-tooltip-target="tooltip-animation" onClick={() => handleButtonClick('TopAirIntake')} type="button" className={` ${buttonStyle} top-[18%] left-[61.75%]`} />
+                            <button data-tooltip-target="tooltip-animation" onClick={() => handleButtonClick('SideAirIntake')} type="button" className={` ${buttonStyle} top-[38%] left-[40%]`} />
+                            <button data-tooltip-target="tooltip-animation" onClick={() => handleButtonClick('Suspension')} type="button" className={` ${buttonStyle} top-[46%] left-[29%]`} />
+                            <button data-tooltip-target="tooltip-animation" onClick={() => handleButtonClick('BrakeCoolingDuct')} type="button" className={` ${buttonStyle} top-[52%] left-[23.5%]`} />
+                            <button data-tooltip-target="tooltip-animation" onClick={() => handleButtonClick('Tyres')} type="button" className={` ${buttonStyle} top-[52%] left-[60%]`} />
+                            <button data-tooltip-target="tooltip-animation" onClick={() => handleButtonClick('FrontWing')} type="button" className={` ${buttonStyle} top-[82%] left-[42%]`} />
+                            <button data-tooltip-target="tooltip-animation" onClick={() => handleButtonClick('RearWing')} type="button" className={` ${buttonStyle} top-[12%] left-[77%]`} />
+                            <button data-tooltip-target="tooltip-animation" onClick={() => handleButtonClick('Body')} type="button" className={` ${buttonStyle} top-[40%] left-[76%]`} />
+                        </div>
                     }
 
                 </div>
--- src/assets/knowMore.js
+++ src/assets/knowMore.js
@@ -3,7 +3,12 @@ const knowMore = {
     Halo: { src: '/basic/halo.png' },
     Tyres: { src: '/basic/tyres.png' },
     SideAirIntake: { src: '/basic/side-airintake.png' },
-    TopAirIntake: { src: '/basic/top-airintake.png' }
+    TopAirIntake: { src: '/basic/top-airintake.png' },
+    RearWing: { src: '/basic/rear-wing.png' },
+    FrontWing: { src: '/basic/front-wing.png' },
+    Body: { src: '/basic/body.png' },
+    BrakeCoolingDuct: { src: '/basic/brake-cooling-duct.png' },
+    Suspension: { src: '/basic/suspension.png' },    
 };
 
 export { knowMore };
