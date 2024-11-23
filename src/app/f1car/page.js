99f83d19fcc000ebf3478388bb03ef5f81c10803
AswinP-Thampan <aswinp.thampan@gmail.com>
2024-11-23 05:34:56 +0530

    Update page.js

--- src/app/f1car/page.js
+++ src/app/f1car/page.js
@@ -9,7 +9,7 @@ export default function Page() {
     const [type, setType] = useState('Click to know more')
     const [src, setSrc] = useState('/basic/b1.jpg')
 
-    const buttonStyle = "bg-site_text bg-opacity-30 hover:bg-red-700 hover:bg-opacity-50 md:border-4 sm:border-2 border-site_text hover:border-site_text hover:border-opacity-60 absolute rounded-full sm:w-10 sm:h-10 md:w-12 md:h-12 2xl:w-14 2xl:h-14 transform -translate-x-1/2 -translate-y-1/2"
+    const buttonStyle = "bg-site_text bg-opacity-30 hover:bg-red-700 hover:bg-opacity-50 border-2 sm:border-2 md:border-4 border-site_text hover:border-site_text hover:border-opacity-60 absolute rounded-full w-7 h-7 sm:w-10 sm:h-10 md:w-12 md:h-12 2xl:w-14 2xl:h-14 transform -translate-x-1/2 -translate-y-1/2"
 
     const handleButtonClick = (type) => {
         // console.log("knowMore[type].src",knowMore[type] , type)
@@ -38,13 +38,13 @@ export default function Page() {
                         <div>Simply move your mouse over the labeled areas, and click it to get a detailed information about that specific part. Whether you&apos;re curious about the aerodynamics of the spoilers or the power behind the engine, this interactive guide will give you a deeper understanding of what makes an F1 car a marvel of engineering.</div>
                     </div>
                 </div>
-                <div className='relative' style={{ position: 'relative', width: '100%', maxWidth: '1200px', margin: '0 auto' }}>
+                <div className='relative w-full h-auto max-h-full-[1200px]' style={{ margin: '0 auto' }}>
                     {type !== 'Click to know more' &&
                         <button onClick={() => handleGoBack()} type="button" className='bg-transparent hover:bg-opacity-50 absolute top-[7%] left-[5%] w-8 h-8 sm:w-12 sm:h-12 lg:w-14 lg:h-14 transform -translate-x-1/2 -translate-y-1/2' >
                             <img src='/icons/back.svg' className='w-7 h-7' />
                         </button>
                     }
-                    <img className={`h-fit rounded-3xl w-full border-8 border-l-0 border-b-0 border-color-`} src={src} alt="Main Image" />
+                    <img className='h-fit rounded-3xl w-full' src={src} alt="Main Image" />
                     {type == 'Click to know more' &&
                         <div>
                             <button data-tooltip-target="tooltip-animation" onClick={() => handleButtonClick('Halo')} type="button" className={` ${buttonStyle} top-[24%] left-[55%]`} />
