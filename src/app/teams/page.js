b08d81d433ae320d736d9a0aef3942b416831d74
AswinP-Thampan <aswinp.thampan@gmail.com>
2024-11-24 02:03:22 +0530

    logos

--- src/app/teams/page.js
+++ src/app/teams/page.js
@@ -5,24 +5,29 @@ import Link from "next/link";
 export default function Home() {
 
     const teams = [
-        { name: "McLaren", src: "https://dl.memuplay.com/new_market/img/mclaren.mobile.icon.2023-02-16-09-21-09.png", href: "/teams/mclaren" },
-        { name: "Red Bull", src: "https://wallpapers.com/images/high/red-bull-pictures-zo19obew3axn6w2g.webp", href: "/teams" },
-        { name: "Mercedes", src: "https://seeklogo.com/images/M/mercedes-amg-f1-logo-1ED622D95E-seeklogo.com.png", href: "/teams/mercedes" },
-        { name: "Aston Martin", src: "https://media.astonmartin.com/wp-content/uploads/2021/01/588489-scaled-1500x1370.jpg", href: "/teams" },
-        { name: "Scuderia Ferrari", src: "https://images.ps-aws.com/c?url=https%3A%2F%2Fd3cm515ijfiu6w.cloudfront.net%2Fwp-content%2Fuploads%2F2023%2F12%2F17092406%2Fferrari-badge.jpg", href: "/teams/ferrari" },
+        { name: "McLaren", src: "/team/McLaren.jpg", href: "/teams/mclaren" },
+        { name: "Red Bull", src: "/team/RedBull.jpg", href: "/teams" },
+        { name: "Mercedes", src: "/team/Mercedes.jpg", href: "/teams/mercedes" },
+        { name: "Aston Martin", src: "/team/Aston.jpg", href: "/teams" },
+        { name: "Scuderia Ferrari", src: "/team/Ferrari.jpg", href: "/teams/ferrari" },
+        { name: "Alpine", src: "/team/Alpine.jpg", href: "/teams" },
+        { name: "Haas", src: "/team/Haas.jpg", href: "/teams" },
+        { name: "Alpha Tauri", src: "/team/AlphaTauri.jpg", href: "/teams" },
+        { name: "Williams", src: "/team/Williams.jpg", href: "/teams" },
+        { name: "Alfa Romeo", src: "/team/Alfa.jpg", href: "/teams" },
     ]
 
     return (
         <div className="justify-around">
             <NavBar />
 
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-8 w-full h-full">
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-8 w-full h-full ">
                 {teams.map((team, index) => (
                     <div key={index} className="flex justify-center grayscale hover:grayscale-0 transition-all duration-700">
                         <Link className="flex flex-col gap-4 items-center md:p-4 py-2 block text-black font-F1R font-semibold" href={team.href}>
                             <div className="w-full h-full relative">
                                 <img
-                                    className="w-full h-full object-cover rounded-3xl cursor-pointer filter "
+                                    className="w-full h-full object-cover rounded-3xl cursor-pointer filter shadow-2xl"
                                     src={team.src}
                                     alt={team.name}
                                 />
